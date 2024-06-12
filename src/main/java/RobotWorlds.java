84f16185e267aac4156f5c46819903bbc3ac4413
jbarker023 <jbarker023@student.wethinkcode.co.za>
2024-06-12 16:26:54 +0200

    minor

---
+++ src/main/java/RobotWorlds.java
@@ -0,0 +1,9 @@
+import Server.Server;
+
+import java.io.IOException;
+
+public class RobotWorlds {
+    public static void main(String[] args) throws IOException {
+        Client.Client.main(new String[0]);
+    }
+}
