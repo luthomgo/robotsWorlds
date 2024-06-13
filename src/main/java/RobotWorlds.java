3d390612b8ecd5c505546d230964f2e5624ec513
jbarker023 <jbarker023@student.wethinkcode.co.za>
2024-06-13 09:25:10 +0200

    install

--- src/main/java/RobotWorlds.java
+++ src/main/java/RobotWorlds.java
@@ -1,9 +1,22 @@
 import Server.Server;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class RobotWorlds {
     public static void main(String[] args) throws IOException {
-        Client.Client.main(new String[0]);
+        if (args.length >= 1){
+            Client.Client.main(args);
+        }
+        else Server.main(new String[0]);
+//        else Server.main(new String[0]);
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Version ?");
+//        String version = s.nextLine();
+//        if (version.equalsIgnoreCase("client")){
+//            Client.Client.main(args);
+//        }
+//        else  Server.main(new String[0]);
+
     }
 }
