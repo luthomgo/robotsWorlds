780b5574f3b2bf61be2827d3d637760c32ffcfd6
duwimana023 <duwimana023@student.wethinkcode.co.za>
2024-06-14 12:10:18 +0200

    RobotWorld DocString

--- src/main/java/RobotWorlds.java
+++ src/main/java/RobotWorlds.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class RobotWorlds {
+    /**
+     * Main method that starts the server application.
+     *
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         if (args.length >= 1){
             Client.Client.main(args);
